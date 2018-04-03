598f40988c4c261a88a8632d354c2875581d09ca
nickmvincent <nickmvincent@gmail.com>
2018-04-03 00:13:30 -0500

    new batch file

---
+++ scripts1.sh
@@ -0,0 +1,19 @@
+python sandbox.py --grouping gender --userfrac 1.0 --ratingfrac 1.0 --dataset ml-1m
+python sandbox.py --grouping gender --userfrac 1.0 --ratingfrac 0.5 --dataset ml-1m
+python sandbox.py --grouping gender --userfrac 0.5 --ratingfrac 1.0 --dataset ml-1m
+
+python sandbox.py --grouping state --userfrac 1.0 --ratingfrac 1.0 --dataset ml-1m
+python sandbox.py --grouping state --userfrac 1.0 --ratingfrac 0.5 --dataset ml-1m
+python sandbox.py --grouping state --userfrac 0.5 --ratingfrac 1.0 --dataset ml-1m
+
+python sandbox.py --grouping age --userfrac 1.0 --ratingfrac 1.0 --dataset ml-1m
+python sandbox.py --grouping age --userfrac 1.0 --ratingfrac 0.5 --dataset ml-1m
+python sandbox.py --grouping age --userfrac 0.5 --ratingfrac 1.0 --dataset ml-1m
+
+python sandbox.py --grouping power --userfrac 1.0 --ratingfrac 1.0 --dataset ml-1m
+python sandbox.py --grouping power --userfrac 1.0 --ratingfrac 0.5 --dataset ml-1m
+python sandbox.py --grouping power --userfrac 0.5 --ratingfrac 1.0 --dataset ml-1m
+
+python sandbox.py --grouping occupation --userfrac 1.0 --ratingfrac 1.0 --dataset ml-1m
+python sandbox.py --grouping occupation --userfrac 1.0 --ratingfrac 0.5 --dataset ml-1m
+python sandbox.py --grouping occupation --userfrac 0.5 --ratingfrac 1.0 --dataset ml-1m
