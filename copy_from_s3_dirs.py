175f5e0a6c52592abddd3ea53a6bd61ef4ea35ef
nickmvincent <nickmvincent@gmail.com>
2018-04-08 21:59:47 -0500

    update notebook work

---
+++ copy_from_s3_dirs.py
@@ -0,0 +1,20 @@
+import os
+import shutil
+
+
+def recursive_search(directory):
+    for root, dirs, files in os.walk(directory):
+        print('root', root)
+        print('dirs', dirs)
+        print('files', files)
+        for file in files:
+            if file.endswith('.csv'):
+                print(file)
+                shutil.copyfile(root + '/' + file, 'results/' + file)
+        for d in dirs:
+            recursive_search(root + '/' + d)
+
+
+if __name__ == '__main__':
+    # put your directory here...
+    recursive_search('C:/Users/Nick/Desktop/mys3/databoycott/round2')
