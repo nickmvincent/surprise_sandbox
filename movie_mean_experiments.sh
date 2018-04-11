1b740a8dbca459aee8d4a0e5b39de09417938d72
nickmvincent <nickmvincent@gmail.com>
2018-04-11 00:06:15 -0500

    movie mean experiments

---
+++ movie_mean_experiments.sh
@@ -0,0 +1,11 @@
+python sandbox.py --grouping sample --sample_sizes 60 --num_samples 500 --movie_mean
+python sandbox.py --grouping sample --sample_sizes 604 --num_samples 500 --movie_mean
+python sandbox.py --grouping sample --sample_sizes 1208 --num_samples 1000 --movie_mean
+python sandbox.py --grouping sample --sample_sizes 1812 --num_samples 1000 --movie_mean
+python sandbox.py --grouping sample --sample_sizes 2416 --num_samples 1000 --movie_mean
+python sandbox.py --grouping sample --sample_sizes 3020 --num_samples 1000 --movie_mean
+python sandbox.py --grouping sample --sample_sizes 3624 --num_samples 1000 --movie_mean
+python sandbox.py --grouping sample --sample_sizes 4228 --num_samples 1000 --movie_mean
+python sandbox.py --grouping sample --sample_sizes 4832 --num_samples 1000 --movie_mean
+python sandbox.py --grouping sample --sample_sizes 5436 --num_samples 1000 --movie_mean
+python sandbox.py --grouping sample --sample_sizes 5980 --num_samples 1000 --movie_mean
--- rand_boycotts.sh
+++
@@ -1,8 +0,0 @@
-python sandbox.py --grouping sample --sample_sizes 1 --num_samples 500 --indices 1,500
-python sandbox.py --grouping sample --sample_sizes 3 --num_samples 1000
-python sandbox.py --grouping sample --sample_sizes 6 --num_samples 1000
-python sandbox.py --grouping sample --sample_sizes 30 --num_samples 1000
-python sandbox.py --grouping sample --sample_sizes 60 --num_samples 1000
-python sandbox.py --grouping sample --sample_sizes 302 --num_samples 1000
-python sandbox.py --grouping sample --sample_sizes 604 --num_samples 1000
-python sandbox.py --grouping sample --sample_sizes 3020 --num_samples 1000
