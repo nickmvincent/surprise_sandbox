02f3a0709517d36aeb4047c51257a7c51bb89de7
nickmvincent <nickmvincent@gmail.com>
2018-04-13 16:02:49 -0500

    split out partial user

---
+++ partial_user_boycott.sh
@@ -0,0 +1,6 @@
+python sandbox.py --grouping gender --userfrac 0.5 --ratingfrac 1.0 --dataset ml-1m --num_samples 10 --indices 1,10
+python sandbox.py --grouping age --userfrac 0.5 --ratingfrac 1.0 --dataset ml-1m --num_samples 10 --indices 1,10
+python sandbox.py --grouping power --userfrac 0.5 --ratingfrac 1.0 --dataset ml-1m --num_samples 10 --indices 1,10
+python sandbox.py --grouping occupation --userfrac 0.5 --ratingfrac 1.0 --dataset ml-1m --num_samples 10 --indices 1,10
+python sandbox.py --grouping genre --userfrac 0.5 --ratingfrac 1.0 --dataset ml-1m --num_samples 10 --indices 1,10
+python sandbox.py --grouping genre_strict --userfrac 0.5 --ratingfrac 1.0 --dataset ml-1m --num_samples 10 --indices 1,10
