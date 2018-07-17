09092caeb3768df0654b0e014709aaacd7e857c9
nickmvincent <nickmvincent@gmail.com>
2018-07-17 10:39:31 -0500

    update trunc script

--- bash_scripts/20m_scripts_truncated.sh
+++ bash_scripts/20m_scripts_truncated.sh
@@ -1,6 +1,4 @@
-python sandbox.py --grouping sample --sample_sizes 27699 --num_samples 10 --indices 1,10 --dataset ml-20m --send_to_out
-python sandbox.py --grouping sample --sample_sizes 41548 --num_samples 10 --indices 1,10 --dataset ml-20m --send_to_out
-python sandbox.py --grouping sample --sample_sizes 55397 --num_samples 10 --indices 1,10 --dataset ml-20m --send_to_out
+python sandbox.py --grouping sample --sample_sizes 14 --num_samples 10 --indices 1,10 --dataset ml-20m --send_to_out
 python sandbox.py --grouping sample --sample_sizes 69246 --num_samples 10 --indices 1,10 --dataset ml-20m --send_to_out
 python sandbox.py --grouping sample --sample_sizes 83096 --num_samples 10 --indices 1,10 --dataset ml-20m --send_to_out
 python sandbox.py --grouping sample --sample_sizes 96945 --num_samples 10 --indices 1,10 --dataset ml-20m --send_to_out
